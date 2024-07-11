7695b0cd79bb0d07f7e72622593c50a2b9a300e1
DreaMeR <DreaMeR@DREAMER>
2024-07-11 11:34:58 +0600

    Добавьте файлы проекта.

---
+++ Cycle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int numberOfRepetitions;
+
+            Console.Write("Введите ваше сообщение: ");
+            message = Console.ReadLine();
+            Console.Write("Введите количество повторений сообщения: ");
+            numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRepetitions; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
